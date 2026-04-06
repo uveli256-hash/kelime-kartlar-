16f9bf9586655a5b28d5f6f69b0248456f699c0e
uveli256-hash <uveli256@gmail.com>
2026-04-06 16:12:44 +0300

    Add files via upload

---
+++ kelime/sw.js
@@ -0,0 +1,11 @@
+const CACHE='kelime-v16';
+const FILES=['./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.url.endsWith('.html')){
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(function(r){ return r||fetch(e.request); }));
+});
