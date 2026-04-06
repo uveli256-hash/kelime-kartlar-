48ac1f6b38027e35a89d3a2c5c7b950fe94fb69a
uveli256-hash <uveli256@gmail.com>
2026-04-06 17:06:20 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kelime-v18';
+const CACHE='kelime-v19';
 const FILES=['./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
