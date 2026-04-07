681322348dc73978acab0fc18763a957b2d35ca1
uveli256-hash <uveli256@gmail.com>
2026-04-07 16:14:41 +0300

    Add files via upload

--- kelime/sw.js
+++ kelime/sw.js
@@ -1,4 +1,4 @@
-const CACHE='kelime-v21';
+const CACHE='kelime-v22';
 const FILES=['./index.html','./manifest.json','./icon.svg','./apple-touch-icon.png','./icon-512.png','./icon-192.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
